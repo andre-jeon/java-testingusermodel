55c5713c629c914985a433487043b5da7c545068
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-09-16 19:16:25 -0400

    first test passed and sanity checked

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -1,15 +1,36 @@
 package com.lambdaschool.usermodel.services;
 
+import com.lambdaschool.usermodel.UserModelApplication;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.channels.spi.SelectorProvider;
 
 import static org.junit.Assert.*;
 
+// use the database
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplication.class)
 public class UserServiceImplTest {
+
+    @Autowired
+    private UserService userService;
 
     @Before
     public void setUp() throws Exception {
+        // mocks -> fake data
+        // stubs -> fake methods
+        // Java -> mocks
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
@@ -22,6 +43,7 @@ public class UserServiceImplTest {
 
     @Test
     public void findByNameContaining() {
+        assertEquals(1, userService.findByNameContaining("barnbarn").size());
     }
 
     @Test
