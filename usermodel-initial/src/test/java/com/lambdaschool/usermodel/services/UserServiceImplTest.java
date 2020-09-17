7b2c4c3eefb040c4561d37412a62f7c18f901a11
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-09-16 22:19:41 -0400

    UserService Test done?

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -1,17 +1,23 @@
 package com.lambdaschool.usermodel.services;
 
 import com.lambdaschool.usermodel.UserModelApplication;
+import com.lambdaschool.usermodel.exceptions.ResourceNotFoundException;
+import com.lambdaschool.usermodel.models.Role;
 import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.nio.channels.spi.SelectorProvider;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -22,6 +28,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UserModelApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceImplTest {
 
     @Autowired
@@ -45,34 +52,59 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void findUserById() {
-        assertEquals("admin", userService.findUserById(4).getUsername());
+    public void a_findUserById() {
+        assertEquals("barnbarn", userService.findUserById(11).getUsername());
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void b_findUserByIdNotFound() {
+        assertEquals("", userService.findUserById(9000).getUsername());
     }
 
     @Test
-    public void findByNameContaining() {
+    public void c_findByNameContaining() {
         assertEquals(2, userService.findByNameContaining("tt").size());
     }
 
     @Test
-    public void findAll() {
+    public void d_findAll() {
         assertEquals(5, userService.findAll().size());
     }
 
     @Test
-    public void delete() {
+    public void e_delete() {
+        userService.delete(4);
+        assertEquals(4, userService.findAll().size());
     }
 
     @Test
-    public void findByName() {
+    public void f_findByName() {
+        assertEquals("barnbarn", userService.findByName("barnbarn").getUsername());
     }
 
     @Test
-    public void save() {
+    public void g_save() {
+        // create an user to save
+        String u4Name = "barnbarn2.0";
+        User u4 = new User("barnbarn2.0",
+                "password",
+                "barnbarnTEST@school.lambda");
+        Role role = new Role("turtle");
+        role.setRoleid(3);
+        u4.getRoles()
+                .add(new UserRoles(u4, role));
+        User addUser = userService.save(u4);
+        assertNotNull(addUser);
+        assertEquals(u4Name, addUser.getUsername());
     }
 
     @Test
-    public void update() {
+    public void gg_saveput() {
+
+    }
+
+    @Test
+    public void h_update() {
     }
 
     @Test
